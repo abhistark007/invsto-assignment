bd20bb1de5094f2640d18197a421d9d9f3087143
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-06-05 12:27:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
+      {/* background div with curve at bottom left */}
+      <div className="app__background">
+        <div className="app__background__circleone"></div>
+        <div className="app__background__circletwo"></div>
+      </div>
+
+      <div className="app__header">
+        <h1 >Simple, traffic-based pricing</h1>
+        <p>Sign-up for our 30-day trial. No credit card required.</p>
+      </div>
+
+
     </div>
   );
 }
